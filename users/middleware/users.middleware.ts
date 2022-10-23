030cd9b0ccb57632474e653687f4ca5a4c4477b1
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-10-23 14:57:24 -0700

    create Node.js REST middleware

---
+++ users/middleware/users.middleware.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import userService from '../services/users.service';
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:users-controllers');
+class UsersMiddleware {
+    
+}
+
+export default new UsersMiddleware();
