ee0f505413d4247c90adb3fe6fa4b9cfc60ea1bc
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-10-23 14:35:47 -0700

    build REST API controller

---
+++ users/controllers/users.controller.ts
@@ -0,0 +1,49 @@
+// express adds types to the request/response obj
+// from controller functions
+import express from 'express';
+
+// The newly created user services
+import usersService from '../services/users.service';
+
+// argon2 lib fro pasword hashing
+import argon2 from 'argon2';
+
+// use debug with a custom context
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:users-controller');
+class UsersController {
+    async listUsers(req: express.Request, res: express.Response) {
+        const users = await usersService.list(100, 0);
+        res.status(200).send(users);
+    }
+    async getUserById(req: express.Request, res: express.Response) {
+        const user = await usersService.readById(req.body.id);
+        res.status(200).send(user);
+    }
+    async createUser(req: express.Request, res: express.Response) {
+        req.body.password = await argon2.hash(req.body.password);
+        const userId = await usersService.create(req.body);
+        res.status(201).send({ id: userId });
+    }
+    async patch(req: express.Request, res: express.Response) {
+        if (req.body.password) {
+            req.body.password = await argon2.hash(req.body.password);
+        }
+        log(await usersService.patchById(req.body.id, req.body));
+        res.status(204).send();
+    }
+    async put(req: express.Request, res: express.Response) {
+        req.body.password = await argon2.hash(req.body.password);
+        log(await usersService.putById(req.body.id, req.body));
+        res.status(204).send();
+    }
+    async removeUser(req: express.Request, res: express.Response) {
+        log(await usersService.deleteById(req.body.id));
+        res.status(204).send();
+    }
+}
+
+export default new UsersController();
+
+
