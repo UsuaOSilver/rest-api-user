a1ae321c76c51dd531d5c8681436c672876a5e5e
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-10-23 01:20:50 -0700

    add create() & get() for UsersDao class

---
+++ users/daos/users.dao.ts
@@ -0,0 +1,27 @@
+import { CreateUserDto } from '../dto/create.user.dto';
+import { PatchUserDto } from '../dto/patch.user.dto';
+import { PutUserDto } from '../dto/put.user.dto';
+import shortid from 'shortid';
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:in-memory-dao');
+
+class UsersDao {
+    users: Array<CreateUserDto> = [];
+    
+    constructor() {
+        log('Crerated new instance of UsersDao');
+    }
+    
+    async addUser(user: CreateUserDto) {
+        user.id = shortid.generate();
+        this.users.push(user);
+        return user.id;
+    }
+    async getUsers() {
+        return this.users;
+    }
+    async getUserById(userId: string) {
+        return this.users.find((user: { id: string }) => user.id === userId);
+    }
+}
