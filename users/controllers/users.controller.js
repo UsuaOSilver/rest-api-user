b479aab2b1a8fb44edc011230019f948fd47898d
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-10-22 23:19:30 -0700

    basic project structure

--- users/controllers/users.controller.js
+++
@@ -1,9 +0,0 @@
-// Controller hashes the password appropriately with definition below
-
-exports.insert = (req, res) => {
-    let salt = crypto.randomBytes(16).toString('based64');
-    let hash = crypto.createHmac('sha512', salt).update(req.body.password).digest("based64");
-    req.body.password = salt + "$" + hash;
-    req.body.permissionLevel = 1;
-    UserModel.createUser(req.body).then((result) => { res.status(201).send({ id: result._id });});
-};
